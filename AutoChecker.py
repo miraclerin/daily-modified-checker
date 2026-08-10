2960f864891d1f3c8086fe30c75a7ff5e7c30e5f
miraclerin <vkdm33@gmail.com>
2026-08-10 12:52:35 +0500

    initial commit
    i don't like git for small projects

---
+++ AutoChecker.py
@@ -0,0 +1,25 @@
+ # This file is part of the daily-edited-checker distribution (https://github.com/miraclerin/daily-edited-checker).
+ # Copyright (c) 2026 miraclerin.
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ #
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from time import sleep, time
+
+from DailyEditedChecker import DailyEditedChecker
+
+checker = DailyEditedChecker()
+
+while True:
+    checker.run()
+    print(f"[DEBUG] Checked! ({time()})")
+    sleep(checker.settings["process.update.time"]) 
---
+++ Visualizer.py
@@ -0,0 +1,164 @@
+ # This file is part of the daily-edited-checker distribution (https://github.com/miraclerin/daily-edited-checker).
+ # Copyright (c) 2026 miraclerin.
+ # 
+ # This program is free software: you can redistribute it and/or modify  
+ # it under the terms of the GNU General Public License as published by  
+ # the Free Software Foundation, version 3.
+ #
+ # This program is distributed in the hope that it will be useful, but 
+ # WITHOUT ANY WARRANTY; without even the implied warranty of 
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ # General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License 
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import datetime
+from time import time
+from colorama import Fore, Style
+from json import JSONDecodeError
+
+from DailyEditedChecker import DailyEditedChecker
+
+class Visualizer():
+    def __init__(self):
+        self.today = datetime.today().strftime("%Y-%m-%d")
+        self.day_of_week = int(datetime.today().strftime("%w"))
+        self.week = int(datetime.today().strftime("%W"))
+        self.year = int(datetime.today().strftime("%Y"))
+        
+        self.checker = DailyEditedChecker()
+        
+        self.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+        
+    def run(self):
+        
+        
+        f = open(self.checker.settings["to_check.path"])
+        to_check = f.read().split("\n")
+        f.close()
+
+        if to_check[0] == "":
+            print(f"\n{Fore.YELLOW}[WARNING] to_check.txt is empty or incorrectly written. Refer to INSTRUCTION.txt to fix it.{Style.RESET_ALL}\n")
+        
+        self.checker.run()
+        
+        print("\n\n")
+        for file_path in to_check:
+            print(f"\n{file_path}:\n")
+            self.print_graph(self.year, self.get_graph(file_path, self.year))
+            print()
+                
+        while True:
+            try:
+                the_year = int(input("Type the year for which you want to view statistics: "))
+
+                if the_year < 1000 or the_year >= 9999:
+                    print(f"\n{Fore.RED}[ERROR] An year must be in range 1000...9998.{Style.RESET_ALL}\n")
+                    continue
+                
+                print("\n\n")
+
+                for file_path in to_check:
+                    print(f"\n{file_path}:\n")
+                    self.print_graph(the_year, self.get_graph(file_path, the_year))
+                    print()
+                
+            except ValueError:
+                print(f"\n{Fore.RED}[ERROR] Don't seem to be an year.{Style.RESET_ALL}\n")
+
+
+    def get_graph(self, file_path, year):
+        self.checker.load_database()
+        data = self.checker.database[file_path]
+
+        week = 1
+
+        is_graph_done = False
+        graph = []
+        
+        while not is_graph_done:
+            for day in range(1, 8):
+                try:
+                    date = datetime.fromisocalendar(year, week, day).strftime("%Y-%m-%d")
+                except ValueError:
+                    if week == 54:
+                            is_graph_done = True
+                            break
+                    elif datetime.fromisocalendar(year, week - 1, 7).strftime("%d") != 31:
+                        date = datetime.fromisocalendar(year + 1, 1, day).strftime("%Y-%m-%d")                        
+                    else:
+                        is_graph_done = True
+                        break
+                if day == 1:
+                    graph.append([])
+
+                if str(year) != date[:date.find("-")]:
+                    graph[week - 1].append("   ") 
+                elif date in data:
+                    graph[week - 1].append("[#]")
+                else:
+                    graph[week - 1].append("[ ]")
+
+            week += 1
+
+        graph = [[j[i] for j in graph] for i in range(7)]
+
+        return graph
+
+
+    def print_graph(self, year, graph):
+        if graph[self.day_of_week - 1][self.week] == "[#]":
+            graph[self.day_of_week - 1][self.week] = f"[{Fore.YELLOW}#{Style.RESET_ALL}]"
+        
+        graph = [[(f"[{Fore.GREEN}#{Style.RESET_ALL}]" if j == "[#]" else j) for j in i] for i in graph]
+
+        week = 0
+        week_month_dict = {1: f"{self.months[0]} 1 "}
+        while True:
+            week += 1
+            try:
+                date = datetime.fromisocalendar(year, week, 1).strftime("%m-%d")
+            except ValueError:
+                break
+            month = int(date[:date.find("-")])
+            day = int(date[date.find("-") + 1:])
+            if day in range(8) and month > 1:
+                week_month_dict[week] = f"{self.months[month - 1]} {day} "
+
+        week_month_str = ""
+        week_counter = 1
+        while week_counter < 54:
+            if week_counter in week_month_dict:
+                week_month_str += week_month_dict[week_counter]
+                week_counter += 2
+            week_month_str += " " * 3
+            week_counter += 1
+            
+        print(f"\t\t{week_month_str}")
+        print("\t\t" + "".join(graph[0]))
+        print("\tTue\t" + "".join(graph[1]))
+        print("\t\t" + "".join(graph[2]))
+        print("\tThu\t" + "".join(graph[3]))
+        print("\t\t" + "".join(graph[4]))
+        print("\tSat\t" + "".join(graph[5]))
+        print("\t\t" + "".join(graph[6]))
+        print()
+        print(f"\t\t\t{Fore.GREEN}Edited\t\t{Fore.YELLOW}Edited today{Style.RESET_ALL}")
+
+
+if __name__ == '__main__':
+    print("Daily Edited Checker Visualizer  Copyright (C) 2026  miraclerin")
+    try:
+        visualizer = Visualizer()
+        visualizer.run()
+    except JSONDecodeError:
+        print(f"\n{Fore.RED}[ERROR] Some file cannot be read.{Style.RESET_ALL}\n")
+        input()
+    except FileNotFoundError:
+        print(f"\n{Fore.RED}[ERROR] Some file cannot be found.{Style.RESET_ALL}\n")
+        input()
+        
+    
