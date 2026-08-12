55fc88a6977bc82b0c36aa2efb8d3a65b460cb16
miraclerin <vkdm33@gmail.com>
2026-08-12 18:38:10 +0500

    Update AutoChecker.py

--- AutoChecker.py
+++ AutoChecker.py
@@ -14,7 +14,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-from time import sleep, time
+from time import sleep
 
 from DailyModifiedChecker import DailyModifiedChecker
 
